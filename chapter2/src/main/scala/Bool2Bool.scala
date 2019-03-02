23cd75450b16a3dbc28e44447e27a44fe1c519d5
Ayush <ayush.mittal@ing.nl>
2019-03-02 18:09:17 +0100

    add bool2bool

---
+++ chapter2/src/main/scala/Bool2Bool.scala
@@ -0,0 +1,17 @@
+/**
+  * @author Ayush Mittal
+  */
+class Bool2Bool {
+
+  /*
+  In terms of category theory i assume there are only 4 arrows possible
+  true -> false
+  false -> true
+  true -> true
+  false -> false
+  f1 and f2 repesent these 4 arrows
+   */
+  def f1(bool: Boolean) = !bool
+
+  def f2(bool: Boolean) = bool
+}
--- chapter2/src/main/scala/Memoize.scala
+++ chapter2/src/main/scala/Memoize.scala
@@ -1,17 +1,12 @@
 /**
  * @author Ayush Mittal
  */
-class Memoize[-A, +B](f: A => B) {
+class Memoize[-A, +B](f: A => B) extends (A => B) {
 
   private[this] val values = scala.collection.mutable.Map.empty[A, B]
 
   def apply(v1: A): B =
-    if (values.contains(v1)) values(v1)
-    else {
-      val result = f(v1)
-      values += ((v1, result))
-      result
-    }
+    values.getOrElseUpdate(v1, f(v1))
 }
 
 object Memoize {
